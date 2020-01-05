bee75282c131c1f066d7fbd2f94f1cb37fc38874
Heather MacGregor <macgregor@ucsb.edu>
2020-01-04 18:01:24 -0800

    Created a Python script that will automate data collection.
    get_envr_data.py calls get_sensor_data.ipynb, which creates a text file read by make_csv_files.ipynb, which in turn creates csv files for each active sensor in the appropriate folder

---
+++ get_envr_data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Script for all sensor data
+
+import sys
+import os
+
+import schedule
+import time
+
+def job():
+    print("I am doing this job!")
+    os.system('runipy get_sensor_data.ipynb')
+    os.system('runipy make_csv_files.ipynb')
+
+
+schedule.every().saturday.at("17:55").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
